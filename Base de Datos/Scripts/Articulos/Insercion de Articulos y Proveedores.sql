98dbf8f16bc44e46c0e1eac71c841e61ea5861dc
Juan D'Esposito <juan.desposito@gmail.com>
2018-11-07 21:27:59 -0300

    Cambios

---
+++ Base de Datos/Scripts/Articulos/Insercion de Articulos y Proveedores.sql
@@ -0,0 +1,10 @@
+insert into proveedor
+(nombre, rubro, ciudad)
+value
+("Producto X", "Limpieza", "Tandil"),
+("Producto Y", "Higiene", "Azul"),
+("Producto Z", "Farmacia", "Bolivar"),
+("Producto W", "Limpieza", "Mar del Plata");
+
+insert into articulo
+(
